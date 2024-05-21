48f097400bc4830601127141bee2cd78ec51974a
AmulyaMurahari <37552813+AmulyaMurahari@users.noreply.github.com>
2024-05-21 00:27:42 -0400

    Added send message component

--- client/src/components/customHeader/index.jsx
+++ client/src/components/customHeader/index.jsx
@@ -6,10 +6,15 @@ const CustomerHeader = ({ chat }) => {
     <div className="chat-header">
       <div className="flexbetween">
         <ChatBubbleLeftRightIcon className="icon-chat" />
-        <h3 className="header-text">{chat.title}</h3>\
+        <h3 className="header-text">{chat.title}</h3>
       </div>
       <div className="flexbetween">
-        
+        <PhoneIcon className='icon-phone' />
+        { chat.description !== "⬅️ ⬅️ ⬅️" ? 
+        (<p className='header-text'>{chat.description}</p>
+      ) : (
+      <p className='header-text'>no chat selected</p>
+      )} 
       </div>
     </div>
   )
