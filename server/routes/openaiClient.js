0c6200a2338aecf73263e27c2eaf06d58a7ce6f8
AmulyaMurahari <37552813+AmulyaMurahari@users.noreply.github.com>
2024-06-06 11:39:51 -0400

    few more code changes related to openai

---
+++ server/routes/openaiClient.js
@@ -0,0 +1,13 @@
+// openaiClient.js
+// import { Configuration, OpenAIApi } from "openai";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const configuration = new Configuration({
+//   apiKey: process.env.OPEN_API_KEY,
+// });
+
+// const openaiClient = new OpenAIApi(configuration);
+
+// export default openaiClient;
